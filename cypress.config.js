fabe986daacae363a931b1148007390fc4e674d9
Dipendra Shakya <shakyadipen@gmail.com>
2025-05-04 04:36:58 +0545

    mochaawesome module

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,59 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  reporter:'cypress-mochawesome-reporter',
+reporterOptions:{
+  mochaFile:'results/my-test-output.xml',
+  reportPageTitle:'Cypress Intuji-Challenge-Report',
+  toConsole:true,
+  charts:true,
+  overwrite:false,
+  embeddedScreenshots:true
+},
+  //
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    
+    testIsolation:false,
+    watchForFileChanges:false,
+    chromeWebSecurity:false,
+    viewportWidth:1000,
+    viewportHeight:600,
+    waitForAnimations:true,
+    animationDistanceThreshold:30,
+    defaultCommandTimeout:6000,
+    execTimeout:60000,
+    pageLoadTimeout:60000,
+    requestTimeout:15000,
+    responseTimeout:15000,
+    video:true,
+    failOnStatusCode:false,
+    
+
+    setupNodeEvents(on,config) {
+      // implement the test run pattern
+// config.specPattern={
+//   'cypress/e2e/tests/RegisterTest.cy.js':
+//   'cypress/e2e/tests/LoginTest.cy.js',
+//   'cypress/e2e/tests/ProductTest.cy.js':
+//   'cypress/e2e/tests/AddToCartTest.cy.js',
+//   'cypress/e2e/tests/CheckoutTest.cy.js':
+// }
+// return config
+     // addMatchImageSnapshotPlugin(on,config);
+      //require('@simonsmith/cypress-image-snapshot/plugin')(on);
+      //return getCompareSnapshotsPlugin(on, config);
+      require('cypress-mochawesome-reporter/plugin')(on);
+     
     },
   },
 });
--- cypress/e2e/tests/002_LoginTest.cy.js
+++ cypress/e2e/tests/002_LoginTest.cy.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import homePage from "../../pageObjects/HomePage.cy";
 import loginPage from "../../pageObjects/LoginPage.cy";
 describe("user should successfully Login and Logout With Valid Credentials",()=>{
@@ -53,64 +52,4 @@ describe("user should successfully Login and Logout With Valid Credentials",()=>
         
 
     })
-        
-
-=======
-import homePage from "../../pageObjects/HomePage.cy";
-import loginPage from "../../pageObjects/LoginPage.cy";
-describe("user should successfully Login and Logout With Valid Credentials",()=>{
-    const loginObj=new loginPage()
-        const homeObj=new homePage()
-    beforeEach(function(){
-        cy.visit("https://automationexercise.com/login");
-        cy.fixture('testData').as('data'); 
-        
-    })
-    it("Should Logout and re-login with proper credentials ",()=>{
-        if(loginObj.isLogoutElementVisible){
-            homeObj.clickLogout()
-            cy.log("User Successfully logged out")
-        }else{
-            cy.get('@data').then(data=>{
-                cy.login(data.email,data.password)
-                cy.log("User Successfully logged In")
-
-                homeObj.clickLogout()
-                cy.log("User Successfully logged out")
-    
-            })
-
-        }
-        //Use fixture to use the data
-        cy.get('@data').then(data=>{
-            cy.login(data.email,data.password)
-
-        })
-        homeObj.clickLogout()
-                cy.log("User Successfully logged out")
-            
-    })
-    it("Should not login with invalid credentials provided",()=>{
-        if(loginObj.isLogoutElementVisible){
-            homeObj.clickLogout()
-            cy.log("User Successfully logged out")
-        }else{
-            cy.get('@data').then(data=>{
-                cy.login(data.email,"WrongPassword")
-                //homeObj.clickLogout()
-                loginObj.getErrorMessage().should('be.visible').contains("Your email or password is incorrect!")
-                })
-        }
-        cy.get('@data').then(data=>{
-            cy.login(data.email,"WrongPassword")
-            loginObj.getErrorMessage().should('be.visible').contains("Your email or password is incorrect!")
-           cy.screenshot()
-
-        })
-        
-
-    })
-        
-
->>>>>>> f36ad6a211603205b5545de1a7e9b20ee848086f
-    })
+})
